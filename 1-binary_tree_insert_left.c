49fbda759bff706eb06aa29aa0fac35560f99d3c
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-11-28 14:49:19 -0800

    fixed bug in insert left implementation

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,28 +13,20 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	struct binary_tree_s *new_node;
 
 	if (parent == NULL)
-        {
-                return (NULL);
-        }
+		return (NULL);
 
 	new_node = malloc(sizeof(struct binary_tree_s));
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
-
-	if (parent != NULL)
-	{
-		new_node->left = parent->left;
-		parent->left->parent = new_node;
-	}
 
 	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
 	new_node->parent = parent;
-
+	new_node->right = NULL;
+	new_node->left = parent->left;
 	parent->left = new_node;
+
+	if (new_node->left)
+		new_node->left->parent = new_node;
 
 	return (new_node);
 }
