be6897fbf9769fd7ac3d9f5a2a5fa7b3d94f190f
SamuelWorld20 <ejalonibuoluwaseyi@gmail.com>
2023-12-01 09:57:12 -0800

    implemented check if binary tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,14 +7,10 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 
-	if ((tree->left == NULL && tree->right != NULL) ||
-		(tree->left != NULL && tree->right == NULL) ||
-		binary_tree_is_full(tree->left) == 0 ||
-		binary_tree_is_full(tree->right) == 0)
-		return (0);
-
-	return (1);
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
