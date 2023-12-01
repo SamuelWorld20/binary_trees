183b31ae3cc1afc444ab557fefd00d67acf226e7
Timmy <akinyemitimilehin044@gmail.com>
2023-11-30 21:18:34 -0800

    Task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,52 +1,32 @@
 #include "binary_trees.h"
 
-binary_tree_t *lowest_common_ancestor_helper(const binary_tree_t *current,
-		const binary_tree_t *first, const binary_tree_t *second);
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
- * @first: Pointer to the first node.
- * @second: Pointer to the second node.
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
  *
  * Return: Pointer to the lowest common
- * ancestor node, or NULL if not found.
+ * ancestor, or NULL if none found
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
+	const binary_tree_t *temp1, *temp2;
+
 	if (!first || !second)
 		return (NULL);
 
-	if (first == second)
-		return ((binary_tree_t *)first);
-
-	return (lowest_common_ancestor_helper(first, second, first));
-}
-
-/**
- * lowest_common_ancestor_helper - Recursive helper function to find LCA.
- * @current: Current node in the recursion.
- * @first: Pointer to the first node.
- * @second: Pointer to the second node.
- *
- * Return: Pointer to the lowest common
- * ancestor node, or NULL if not found.
- */
-binary_tree_t *lowest_common_ancestor_helper(const binary_tree_t *current,
-		const binary_tree_t *first, const binary_tree_t *second)
-{
-	binary_tree_t *l, *r;
-
-	if (!current)
-		return (NULL);
-
-	if (current == first || current == second)
-		return ((binary_tree_t *)current);
-
-	l = lowest_common_ancestor_helper(current->left, first, second);
-	r = lowest_common_ancestor_helper(current->right, first, second);
-
-	if (l && r)
-		return ((binary_tree_t *)current);
-
-	return (l ? l : r);
+	temp1 = first;
+	while (temp1)
+	{
+		temp2 = second;
+		while (temp2)
+		{
+			if (temp1 == temp2)
+				return ((binary_tree_t *)temp1);
+			temp2 = temp2->parent;
+		}
+		temp1 = temp1->parent;
+	}
+	return (NULL);
 }
